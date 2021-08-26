cb9a1aa46103691424c98d82073be26b9116be11
Mário Júnior <83978866+mariojuniordev@users.noreply.github.com>
2021-08-26 09:18:25 -0300

    Add files via upload

---
+++ Exercicio 1.2 (Fatorial)/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    int i;
+
+    printf("Digite um número: ");
+    scanf("%d", x);
+
+    for (int i = (x-1);i > 0; i--){
+        x = x*i;
+    }
+
+    printf("O fatorial do número digitado é: %d", x);
+
+    return 0;
+}
